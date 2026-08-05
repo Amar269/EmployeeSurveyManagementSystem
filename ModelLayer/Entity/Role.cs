8986438899ed07ab691f9cfed913d737c6c1f782
AMARNATH KOLLA <amarrama269@gmail.com>
2026-08-05 15:39:13 +0530

    Added User and Role entity

---
+++ ModelLayer/Entity/Role.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer.Entity
+{
+    public class Role
+    {
+        public int RoleId { get; set; }
+
+        public string RoleName { get; set; } = string.Empty;
+
+        
+        public ICollection<User> Users { get; set; } = new List<User>();
+    }
+}
--- ModelLayer/Entity/User.cs
+++ ModelLayer/Entity/User.cs
@@ -6,8 +6,26 @@ using System.Threading.Tasks;
 
 namespace ModelLayer.Entity
 {
-    internal class User
+    public class User
     {
+        public int UserId { get; set; }
 
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+
+        public int RoleId { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public Role Role { get; set; } = null!;
     }
 }

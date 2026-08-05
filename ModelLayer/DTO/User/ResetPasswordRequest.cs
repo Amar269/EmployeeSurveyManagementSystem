c63f05e7bcd97e5ac292f22929f8848b07ae8a7f
AMARNATH KOLLA <amarrama269@gmail.com>
2026-08-05 17:12:32 +0530

    added  user authentication DTOs

---
+++ ModelLayer/DTO/User/ResetPasswordRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace ModelLayer.DTO.User
+{
+    public class ResetPasswordRequest
+    {
+        [Required]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string NewPassword { get; set; } = string.Empty;
+
+        [Required]
+        [Compare("NewPassword")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
---
+++ ModelLayer/DTO/User/UserResponse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer.DTO.User
+{
+    public class UserResponse
+    {
+        public int UserId { get; set; }
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Role { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}

6b1491d67ee956201538c104b71f513cdd69cfe0
AMARNATH KOLLA <amarrama269@gmail.com>
2026-08-06 14:03:09 +0530

    Added JWT configuration and token generator helper

--- EmployeeSurveyManagementSystem/Controllers/UserController.cs
+++ EmployeeSurveyManagementSystem/Controllers/UserController.cs
@@ -1,6 +1,33 @@
-﻿namespace EmployeeSurveyManagementSystem.Controllers
+﻿using BusinessLogicLayer.Interface;
+using Microsoft.AspNetCore.Mvc;
+using ModelLayer.DTO.User;
+
+namespace EmployeeSurveyManagementSystem.Controllers
 {
-    public class UserController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
     {
+        private readonly IUserBLL _userBLL;
+
+        public UserController(IUserBLL userBLL)
+        {
+            _userBLL = userBLL;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterRequest request)
+        {
+            var result = await _userBLL.RegisterUser(request);
+
+            return Ok(new
+            {
+                success = true,
+                message = "User Registered Successfully",
+                data = result
+            });
+        }
     }
+
+
 }

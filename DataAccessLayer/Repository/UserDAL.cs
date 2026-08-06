8a223a10ea7a155032d6808a36442130a5accead
AMARNATH KOLLA <amarrama269@gmail.com>
2026-08-06 11:24:06 +0530

    implement user repository methods

--- DataAccessLayer/Repository/UserDAL.cs
+++ DataAccessLayer/Repository/UserDAL.cs
@@ -3,10 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataAccessLayer.Context;
+using DataAccessLayer.Interface;
+using Microsoft.EntityFrameworkCore;
+using ModelLayer.Entity;
 
 namespace DataAccessLayer.Repository
 {
-    internal class UserDAL
+    public class UserDAL : IUserDAL
     {
+        private readonly ApplicationDbContext _context;
+
+        public UserDAL(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task AddUser(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public Task<Role?> GetRoleByName(string roleName)
+        {
+            return await _context.Roles
+        .FirstOrDefaultAsync(r => r.RoleName == roleName);
+        }
+
+        public Task<User?> GetUserByEmail(string email)
+        {
+            return await _context.Users
+        .Include(u => u.Role)
+        .FirstOrDefaultAsync(u => u.Email == email);
+        }
+        }
     }
 }
+
+

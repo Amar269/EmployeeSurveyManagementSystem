c881688b2bbdce414acf40454cb844ba67fd7a27
AMARNATH KOLLA <amarrama269@gmail.com>
2026-08-09 18:29:36 +0530

    Add survey list DTO

---
+++ ModelLayer/DTO/Survey/SurveyListDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer.DTO.Survey
+{
+    public class SurveyListDto
+    {
+        public int SurveyId { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+    }
+}

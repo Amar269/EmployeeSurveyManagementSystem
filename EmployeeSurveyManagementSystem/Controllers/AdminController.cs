9439046bcae5512cfbe9b38f57c4e5acb448f368
AMARNATH KOLLA <amarrama269@gmail.com>
2026-08-10 15:39:13 +0530

    Add admin responses endpoint

---
+++ EmployeeSurveyManagementSystem/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BusinessLogicLayer.Interface;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace EmployeeSurveyManagementSystem.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize(Roles = "Admin")]
+    public class AdminController : ControllerBase
+    {
+        private readonly IAdminService _adminService;
+
+        public AdminController(IAdminService adminService)
+        {
+            _adminService = adminService;
+        }
+
+        [HttpGet("responses")]
+        public async Task<IActionResult> GetAllResponses()
+        {
+            var responses = await _adminService.GetAllResponsesAsync();
+
+            return Ok(responses);
+        }
+    }
+}

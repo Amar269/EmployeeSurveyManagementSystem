65d0904d94723b774d64029e5657f6110e28cb46
AMARNATH KOLLA <amarrama269@gmail.com>
2026-08-10 14:51:09 +0530

    Implement admin response service

---
+++ BusinessLogicLayer/Service/AdminService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLogicLayer.Interface;
+using DataAccessLayer.Interface;
+using ModelLayer.DTO.Admin;
+
+namespace BusinessLogicLayer.Service
+{
+    public class AdminService : IAdminService
+    {
+        private readonly IAdminRepository _adminRepository;
+
+        public AdminService(IAdminRepository adminRepository)
+        {
+            _adminRepository = adminRepository;
+        }
+
+        public async Task<List<AdminResponseDto>> GetAllResponsesAsync()
+        {
+            return await _adminRepository.GetAllResponsesAsync();
+        }
+    }
+}

d96772cadb42b024a4229b89f5d5fa96771ff3e5
AMARNATH KOLLA <amarrama269@gmail.com>
2026-08-10 13:40:58 +0530

    Add admin repository interface

---
+++ DataAccessLayer/Interface/IAdminRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModelLayer.DTO.Admin;
+
+namespace DataAccessLayer.Interface
+{
+    public interface IAdminRepository
+    {
+
+    }
+}

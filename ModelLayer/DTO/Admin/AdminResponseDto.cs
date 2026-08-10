a7f75fc459bfbfedebb730014465bba495ef34ee
AMARNATH KOLLA <amarrama269@gmail.com>
2026-08-10 13:37:38 +0530

    Add admin response DTO

---
+++ ModelLayer/DTO/Admin/AdminResponseDto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer.DTO.Admin
+{
+    public class AdminResponseDto
+    {
+        public int ResponseId { get; set; }
+
+        public int UserId { get; set; }
+
+        public string EmployeeName { get; set; } = string.Empty;
+
+        public string Email { get; set; }
+
+        public int SurveyId { get; set; }
+
+        public string SurveyTitle { get; set; } = string.Empty;
+
+        public int QuestionId { get; set; }
+
+        public string QuestionText { get; set; } = string.Empty;
+
+        public string Answer { get; set; } = string.Empty;
+
+        public  DateTime SubmittedAt  { get; set; }
+
+
+    }
+}
